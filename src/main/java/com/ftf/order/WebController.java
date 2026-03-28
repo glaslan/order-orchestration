3a830bfdcd36615458506b2d2a4b646d45f2d740
Brooke Cronin <60623477+brookec123@users.noreply.github.com>
2026-03-27 20:54:20 -0400

    set up customizable bootstrap, thymeleaf, and started products page. can run swingboot app and show index.html file.

---
+++ src/main/java/com/ftf/order/WebController.java
@@ -0,0 +1,16 @@
+package com.ftf.order;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+    @GetMapping("/")
+    public String index(Model model) {
+        // model.addAttribute("variableNameInHtml", "ValueToDisplay")
+
+        return "index";
+    }
+}
