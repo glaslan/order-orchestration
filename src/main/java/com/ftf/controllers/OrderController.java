e9b243bec2820b770daef5001f17c8993bf9870e
Andrew Abrantes <andrewabr05@gmail.com>
2026-04-03 18:19:31 -0400

    updating item class, adding remove from cart route and adding test model product list returned to the index page in the UIController

--- src/main/java/com/ftf/controllers/OrderController.java
+++ src/main/java/com/ftf/controllers/OrderController.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.ftf.order.Item;
@@ -13,19 +14,46 @@ import jakarta.servlet.http.HttpSession;
 @Controller
 public class OrderController {
     
-    @GetMapping("/add-to-cart")
-    public String AddToCart(@RequestParam String name, @RequestParam double price, @RequestParam int quantity, HttpSession session) {
+    @PostMapping("/addToCart")
+    public String AddToCart(@RequestParam String name, @RequestParam double price, @RequestParam int quantity, @RequestParam int itemId, HttpSession session) {
         HashMap<String, Item> cart = (HashMap<String, Item>)session.getAttribute("cart");
         if (cart == null) {
             cart = new HashMap<String, Item>();
         }
 
-        cart.put("name", new Item(price, quantity));
+        cart.put(name, new Item(name, price, quantity, itemId));
         session.setAttribute("cart", cart);
 
         // TODO also need to add it to the order manifest db
 
         // TODO I probably need to change this 
+        return "success";
+    }
+
+    @PostMapping("/removeFromCart")
+    public String RemoveFromCart(@RequestParam String name, @RequestParam int quantity, HttpSession session) {
+        HashMap<String, Item> cart = (HashMap<String, Item>)session.getAttribute("cart");
+        if (cart == null) {
+            return "failure";
+        }
+
+        Item item = cart.get(name);
+        if (item != null) {
+            // if the quantity is less then the amount of cart then decrement the amount in the cart
+            if (item.getQuantity() > quantity) {
+                item.setQuantity(item.getQuantity() - quantity);
+                cart.replace(name, item);
+            }
+            // if quantity in cart is equal or less than amount from params then we can just remove it
+            else {
+                cart.remove(name);
+            }
+
+        }
+        session.setAttribute("cart", cart);
+
+        // TODO also need to remove it from the order manifest db
+
         return "success";
     }
 }
